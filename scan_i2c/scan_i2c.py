0e17736de98896fcab30fb67c3ec2f06cb61b2d7
Pat Hanrahan <hanrahan@cs.stanford.edu>
2025-04-29 07:40:02 -0700

    Added i2c pins for rp2040 zero

--- scan_i2c/scan_i2c.py
+++ scan_i2c/scan_i2c.py
@@ -1,7 +1,10 @@
 from machine import Pin, I2C
 
-# i2c = I2C(sda=Pin(6), scl=Pin(7), freq=100_000)
-i2c = I2C(1, sda=Pin(6), scl=Pin(7), freq=100_000)
+# xiao RP2040
+# i2c = I2C(1, sda=Pin(6), scl=Pin(7), freq=100_000)
+
+# rp2040 zero
+i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=100_000)
 
 devices = i2c.scan()
 print("devices: ")
