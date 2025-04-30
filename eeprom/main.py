c7009e0fc8941c8be809ee0741d217d2b4abbe01
Pat Hanrahan <hanrahan@cs.stanford.edu>
2025-04-30 10:29:01 -0700

    updates to eeprom and scan

--- eeprom/main.py
+++ eeprom/main.py
@@ -2,14 +2,15 @@ import time
 from machine import I2C, Pin
 from eeprom import EEPROM
 
-I2C_ADDR = 0x50     # DEC 80, HEX 0x50
-EEPROM_SIZE = 512   # AT24C32 on 0x50
+I2C_ADDR = 0x50     # HEX 0x50
+EEPROM_SIZE = 512   # AT24C512
 
-# define custom I2C interface, default is 'I2C(0)'
-# check the docs of your device for further details and pin infos
-# i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=800000)
 # For RP2040 Xiao use pins 7 and 6 for I2C bus 1
-i2c = I2C(1, sda=Pin(6), scl=Pin(7))
+# i2c = I2C(1, sda=Pin(6), scl=Pin(7))
+
+# rp2040 zero
+i2c = I2C(1, sda=Pin(26), scl=Pin(27))
+
 eeprom = EEPROM(addr=I2C_ADDR, at24x=EEPROM_SIZE, i2c=i2c)
 
 while True:
--- scan_i2c/scan_i2c.py
+++ scan_i2c/scan_i2c.py
@@ -1,10 +1,10 @@
 from machine import Pin, I2C
 
 # xiao RP2040
-# i2c = I2C(1, sda=Pin(6), scl=Pin(7), freq=100_000)
+# i2c = I2C(1, sda=Pin(6), scl=Pin(7))
 
 # rp2040 zero
-i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=100_000)
+i2c = I2C(1, sda=Pin(26), scl=Pin(27))
 
 devices = i2c.scan()
 print("devices: ")
