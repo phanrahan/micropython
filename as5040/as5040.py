41b6854a0ecf005eb249ba18c6aeba7f22aa76e5
Patrick Hanrahan <hanrahan@cs.stanford.edu>
2026-05-19 09:31:08 -0700

    Debugged as5040.py

--- as5040/as5040.py
+++ as5040/as5040.py
@@ -6,49 +6,67 @@ csn = machine.Pin(1, machine.Pin.OUT)
 clk = machine.Pin(2, machine.Pin.OUT)
 do = machine.Pin(4, machine.Pin.IN)
 
-# Initialize states
-csn.value(1) # CSn High (Inactive)
-clk.value(1) # Clock High
+INC = 1
+DEC = 2
 
-def read_as5040():
+# The raw 16-bit packet: [Angle(10bit) | OCF | COF | LIN | DEC | INC | PAR]
+def decode(data, ones):
+
+    # Extract 10-bit angle (bits 15-6)
+    angle = (data >> 6) & 0x03FF
+
+    # magnet not detected if INC and DEC are high
+    mag = (data >> INC) and (data >> DEC)
+    
+    # even parity
+    err = (ones % 2) != 0
+
+    return angle, mag, err
+
+def read_as5040_data():
+    # Initialize states
+    csn.value(1) # CSn High (Inactive)
+    clk.value(1)
+    time.sleep_us(1)
     # CSn low to start transfer
     csn.value(0)
+    time.sleep_us(1)
+    clk.value(0)
     time.sleep_us(1)
     
     # Read 16 bits (Data + Status)
     data = 0
     ones = 0
     for i in range(16):
-        clk.value(0)
+        clk.value(1)
         time.sleep_us(1)
         bit = do.value()
         ones = ones + bit
         data = (data << 1) | bit
-        clk.value(1)
+        clk.value(0)
         time.sleep_us(1)
     
     # CSn high to end transfer
     csn.value(1)
+
+    return data, ones
     
-    # Extract 10-bit angle (bits 15-6)
-    # The raw 16-bit packet: [Angle(10bit) | MAG | DEC | INC | PAR | ERR]
-    angle = (data >> 6) & 0x03FF
-    
-    # Extract status bits
-    err = ones != 0
-    mag = (data >> 4) and (data >> 3)
-    
-    return angle, mag, err
+def read_as5040():
+    while True:
+        data, ones = read_as5040_data()
+        angle, mag, err = decode(data, ones)
+        if not err:
+            break
+    return angle, mag
+
 
 # Main loop
 while True:
     try:
-        angle, mag, err = read_as5040()
+        angle, mag = read_as5040()
         
-        if err:
-            print("Parity error detected!")
-        elif not mag:
-            print("Magnet missing/weak")
+        if not mag:
+            print("Magnet not detected")
         else:
             # Convert 0-1023 to 0-359 degrees
             degrees = (angle * 360) / 1024
